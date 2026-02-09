b1fc35070cb2bc2b2d6da350198c9ba2fa3053f4
harsha27-a11y <harshavardhanmmurugesan@gmail.com>
2026-02-09 14:57:19 +0530

    Change API base URL to new endpoint

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://todo-demo-backend-oyhf.onrender.com/api/todos",
+  baseURL: "https://fsp-c1xu.onrender.com",
 });
 
 export default api;
